153405dcf501a539771b57438b9d816a4816af1b
Пляскин Даниил <zelcamn@yandex.ru>
2021-12-10 21:31:17 +0400

    result first day

--- main.py
+++ main.py
@@ -5,28 +5,50 @@ app = Flask(__name__)
 GlobaldataArd = ''
 GlobaldataSoft = ''
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 @app.route('/index')
 def index():
+    print(request.method)
     return "Привет, Яндекс!"
 
 
-@app.route('/DataArduino/', methods=['POST', 'GET'])
+@app.route('/dataArduino/', methods=['POST', 'GET'])
 def DataArduino():
-    if request.method == 'POST':
+    if request.method == 'GET':
         global GlobaldataSoft
         data = GlobaldataSoft
         return data
-    elif request.method == 'GET':
-        global Globaldata
-        data = GlobaldataArd
-        print(request.form['data'])
+    elif request.method == 'POST':
+        global GlobaldataArd
+        GlobaldataArd = request.get_data()
+        print(request.get_data())
         return "success"
 
 
+@app.route('/dataSoft', methods=['POST', 'GET'])
 def dataSoft():
-    return req
+    print(request.method)
+    if request.method == 'GET':
+        global GlobaldataSoft
+        data = GlobaldataArd
+        # return "test"
+        return data
+    elif request.method == 'POST':
+        global GlobaldataSoft
+        GlobaldataSoft = request.get_data()
+        print(request.get_data())
+        return "success"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print(request.method)
+    if request.method == 'POST':
+        print("post")
+    else:
+        print("get")
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='192.168.88.248')
